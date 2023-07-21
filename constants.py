f76f9eb89487827c39a8167e953be543e76b1791
hxue98 <35674949+hxue98@users.noreply.github.com>
2023-07-20 23:50:21 -0400

    Added constants file FOR DEMO

---
+++ constants.py
@@ -0,0 +1,7 @@
+# This file should be included in .gitignore and not be commited to public repositories but I'm including this for demo purpose
+apiKey = 'SEAWVZ7J5OGVHWQU'
+
+dbHost = "localhost"
+dbUser = "root"
+dbName = "stocks"
+dbPassword = "1234"
